e6fa8671d331656fd111c6d01e277eea789390b2
Ariiq354 <realgoldcraft@gmail.com>
2026-07-23 09:04:43 +0700

    chore: update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
+
+  future: {
+    compatibilityVersion: 5,
+  },
+
   modules: [
     "nuxt-security",
     "@nuxt/image",
