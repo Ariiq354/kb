9c40f9b1356c1d5982c23746a2f16f3bfdc9d6c6
Ariiq354 <realgoldcraft@gmail.com>
2026-08-05 22:00:42 +0700

    fix: try to fix map

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,23 @@ export default defineNuxtConfig({
     "nuxt-charts",
     "nuxt-maplibre",
   ],
+  build: {
+    transpile: ["nuxt-maplibre"],
+  },
+
   css: ["~/assets/css/main.css"],
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        "maplibre-gl",
+        "@indoorequal/vue-maplibre-gl",
+      ],
+    },
+    ssr: {
+      noExternal: ["maplibre-gl", "@indoorequal/vue-maplibre-gl"],
+    },
+  },
 
   ui: {
     colorMode: false,
